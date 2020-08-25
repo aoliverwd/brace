7bcd9f6d09de058ffc9e67a5f0293b869ec8d9ae
Alex Oliver <alexoliverwd@gmail.com>
2020-08-25 15:47:53 +0100

    Test unit test

--- tests/tests/variables/variableTest.php
+++ tests/tests/variables/variableTest.php
@@ -19,7 +19,7 @@
         public function testVariables(): void{
             $brace = new brace\parser;
             $this->assertEquals(
-                "Hello Davee\n",
+                "Hello Dave\n",
                 $brace->process_input_string('Hello {{name}}', ['name' => 'Dave'], false)->return()
             );
         }
