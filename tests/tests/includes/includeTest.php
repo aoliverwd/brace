9210aabcefee99cfb2a383392967281101e59253
Alex Oliver <alexoliverwd@gmail.com>
2021-09-26 12:08:33 +0100

    Check include template from parent path

--- tests/tests/includes/includeTest.php
+++ tests/tests/includes/includeTest.php
@@ -4,19 +4,12 @@
 
     /** Declare strict types */
     declare(strict_types=1);
-    
+
     /** PHPUnit namespace */
     use PHPUnit\Framework\TestCase;
 
-    /**
-     * VariablesTest class
-     */
     final class IncludeTest extends TestCase{
 
-        /**
-         * [testIncludeTemplate description]
-         * @return [type] [description]
-         */
         public function testIncludeTemplate(): void{
             $brace = new brace\parser;
             $brace->template_path = __DIR__.'/';
@@ -26,10 +19,6 @@
             );
         }
 
-        /**
-         * [testIncludeMultipleTemplates description]
-         * @return [type] [description]
-         */
         public function testIncludeMultipleTemplates(): void{
             $brace = new brace\parser;
             $brace->template_path = __DIR__.'/';
@@ -39,16 +28,22 @@
             );
         }
 
-        /**
-         * [testIncludeMultipleIncludes description]
-         * @return [type] [description]
-         */
         public function testIncludeMultipleIncludes(): void{
             $brace = new brace\parser;
             $brace->template_path = __DIR__.'/';
             $this->assertEquals(
                 "Hello ,Welcome",
                 $brace->parse_input_string('[@include include-file] [@include include-file-two]', [], false)->return()
+            );
+        }
+
+        public function testIncludeFromParentPath(): void{
+            $brace = new brace\parser;
+            $brace->template_path = __DIR__.'/';
+
+            $this->assertEquals(
+                "Foo bar",
+                $brace->parse_input_string('[@include ../include-from-parent/include]', [], false)->return()
             );
         }
     }
