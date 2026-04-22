2f73101afd59585111c77730394e866859e0b37c
Alex Oliver <alexoliverwd@gmail.com>
2026-04-22 18:48:43 +0100

    test: add tests for nested and inline condition filters

--- tests/unit/filters/filterTest.php
+++ tests/unit/filters/filterTest.php
@@ -40,4 +40,46 @@ final class FilterTest extends TestCase
                 ->return(),
         );
     }
+
+    public function testFilterNested(): void
+    {
+        $brace = new Brace\Parser();
+        $brace->registerFilter('int', fn($content) => (int) $content);
+
+        $this->assertEquals(
+            1,
+            $brace
+                ->parseInputString(
+                    '{{ number->one|int }}',
+                    [
+                        'number' => [
+                            'one' => '1',
+                        ],
+                    ],
+                    false,
+                )
+                ->return(),
+        );
+    }
+
+    public function testFilterInlineCondition(): void
+    {
+        $brace = new Brace\Parser();
+        $brace->registerFilter('number', fn($content) => number_format((int) $content, 1));
+
+        $this->assertEquals(
+            'true' . PHP_EOL,
+            $brace
+                ->parseInputString(
+                    '{{ number->one|number === 1,123.0 ? "true" : "false" }}',
+                    [
+                        'number' => [
+                            'one' => '1123',
+                        ],
+                    ],
+                    false,
+                )
+                ->return(),
+        );
+    }
 }
